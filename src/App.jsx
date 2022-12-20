80a8ee5e0ce5694d5b90a18737a69e0a7845a820
Sohaib Mao <sohaib.mmeo@devsinc.com>
2022-12-20 14:54:43 +0500

    main general things

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Grids from './components/Grids';
+
+function App() {
+  return (
+    <div className="App">
+      <Grids />
+    </div>
+  );
+}
+
+export default App;
